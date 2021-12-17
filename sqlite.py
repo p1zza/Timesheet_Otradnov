a5c4826e434cec067c6bb8fba578e6d5f3e9ece8
AnaLGiN83 <zyk83@mail.ru>
2021-12-17 18:56:40 +0300

    Добавлена полная схема БД

--- sqlite.py
+++ sqlite.py
@@ -37,10 +37,82 @@ class Database:
 
     def _create_schema(self):
         self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS user_data (
-        login TEXT,
-        password TEXT,
-        role TEXT
-        )''')
+            uid NUMERIC PRIMARY KEY ASC,
+            login TEXT UNIQUE,
+            password TEXT,
+            role TEXT
+            )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS room_types (
+                id NUMERIC PRIMARY KEY ASC,
+                type TEXT UNIQUE
+                )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS rooms (
+                id NUMERIC PRIMARY KEY ASC,
+                type_id NUMERIC,
+                campus TEXT,
+                number NUMERIC,
+                FOREIGN KEY(type_id) REFERENCES room_types(id)
+                )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS personal_data (
+                        id NUMERIC PRIMARY KEY ASC,
+                        name TEXT,
+                        surname TEXT,
+                        patronymic TEXT,
+                        email TEXT,
+                        phone TEXT,
+                        photo BLOB
+                        )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS lecturers (
+                                        personal_id NUMERIC,
+                                        FOREIGN KEY(personal_id) REFERENCES personal_data(id)
+                                        )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS groups (
+                                                id NUMERIC PRIMARY KEY ASC,
+                                                name TEXT UNIQUE
+                                                )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS subjects (
+                                                        id NUMERIC PRIMARY KEY ASC,
+                                                        name TEXT
+                                                        )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS class_types (
+                                                        id NUMERIC PRIMARY KEY ASC,
+                                                        name TEXT UNIQUE
+                                                        )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS teach_plan (
+                                                                id NUMERIC PRIMARY KEY ASC,
+                                                                group_id NUMERIC,
+                                                                subject_id NUMERIC,
+                                                                class_type_id NUMERIC,
+                                                                hours NUMERIC,
+                                                                FOREIGN KEY (group_id) REFERENCES groups(id),
+                                                                FOREIGN KEY (subject_id) REFERENCES subjects(id),
+                                                                FOREIGN KEY (class_type_id) REFERENCES class_types(id)
+                                                                )''')
+
+        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS timesheet (
+                                                                id NUMERIC PRIMARY KEY ASC,
+                                                                room_id NUMERIC,
+                                                                lecturer_id NUMERIC,
+                                                                group_id NUMERIC,
+                                                                class_type_id NUMERIC,
+                                                                subject_id NUMERIC,
+                                                                date DATE,
+                                                                class_number NUMERIC,
+                                                                FOREIGN KEY (room_id) REFERENCES rooms(id),
+                                                                FOREIGN KEY (lecturer_id) REFERENCES lecturers(personal_id),
+                                                                FOREIGN KEY (group_id) REFERENCES groups(id),
+                                                                FOREIGN KEY (subject_id) REFERENCES subjects(id),
+                                                                FOREIGN KEY (class_type_id) REFERENCES class_types(id)
+                                                                )''')
+
         self.main_cursor.execute(f"INSERT INTO user_data(login, password, role) VALUES('admin', "
                                  f"'{sha256('admin'.encode('utf-8')).hexdigest()}', 'admin')")
         self.main_cursor.execute(f"INSERT INTO user_data(login, password, role) VALUES('laborant', "
@@ -59,7 +131,7 @@ class Database:
     def load_rooms(self):
         rooms = []
         result = self.main_cursor.execute("SELECT rooms.id, campus, number, room_types.name FROM rooms "
-                                          "LEFT JOIN room_types ON rooms.id = room_types.id")
+                                          "LEFT JOIN room_types ON rooms.type_id = room_types.id")
         for row in result:
             rooms.append(data_models.Room(result[0], result[1], result[2], result[3]))
         return rooms
@@ -69,8 +141,8 @@ class Database:
         result = result.fetchone()
         if len(result) != 1:
             raise sqlite3.Error("Внутренняя ошибка базы данных")
-        self.main_cursor.execute(f"INSERT INTO rooms(id, campus, number, type) VALUES(?, ?, ?, ?)",
-                                 room.id, room.campus, room.number, room.type)
+        self.main_cursor.execute(f"INSERT INTO rooms(id, campus, number, type_id) VALUES(?, ?, ?, ?)",
+                                 room.id, room.campus, room.number, room.type)  # FIXME: Поправить под модель данных тип-аудитория, когда она будет определена
         self._connection.commit()
 
     def load_personals(self):
@@ -83,7 +155,7 @@ class Database:
         return personals
 
     def add_personal(self, personal_data):
-        self.main_cursor.execute(f"INSERT INTO personal_data(name, surname, patronymic, email, phone, photo) VALUES(?, ?, ?, ?)",
+        self.main_cursor.execute(f"INSERT INTO personal_data(name, surname, patronymic, email, phone, photo) VALUES(?, ?, ?, ?, ?, ?, ?)",
                                  personal_data.name, personal_data.surname, personal_data.patronymic,
                                  personal_data.email, personal_data.phone, personal_data.photo)
         self._connection.commit()

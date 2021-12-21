e892dd72f1308079351bf8fdd55d0f9b52a3ae3f
AnaLGiN83 <zyk83@mail.ru>
2021-12-21 19:47:45 +0300

    Перенос схемы БД в отдельный файл

--- sqlite.py
+++ sqlite.py
@@ -46,86 +46,13 @@ class Database:
 
     def _check_schema(self):
         result = self.main_cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='user_data'")
-        return len(result.fetchone()) > 0
+        result = result.fetchone()
+        return result and len(result) > 0
 
     def _create_schema(self):
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS user_data (
-            uid NUMERIC PRIMARY KEY ASC,
-            login TEXT UNIQUE,
-            password TEXT,
-            role TEXT
-            )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS room_types (
-                id NUMERIC PRIMARY KEY ASC,
-                type TEXT UNIQUE
-                )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS rooms (
-                id NUMERIC PRIMARY KEY ASC,
-                type_id NUMERIC,
-                campus TEXT,
-                number NUMERIC,
-                FOREIGN KEY(type_id) REFERENCES room_types(id)
-                )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS personal_data (
-                        id NUMERIC PRIMARY KEY ASC,
-                        name TEXT,
-                        surname TEXT,
-                        patronymic TEXT,
-                        email TEXT,
-                        phone TEXT,
-                        photo BLOB
-                        )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS lecturers (
-                                        personal_id NUMERIC,
-                                        FOREIGN KEY(personal_id) REFERENCES personal_data(id)
-                                        )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS groups (
-                                                id NUMERIC PRIMARY KEY ASC,
-                                                name TEXT UNIQUE
-                                                )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS subjects (
-                                                        id NUMERIC PRIMARY KEY ASC,
-                                                        name TEXT
-                                                        )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS class_types (
-                                                        id NUMERIC PRIMARY KEY ASC,
-                                                        name TEXT UNIQUE
-                                                        )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS teach_plan (
-                                                                id NUMERIC PRIMARY KEY ASC,
-                                                                group_id NUMERIC,
-                                                                subject_id NUMERIC,
-                                                                class_type_id NUMERIC,
-                                                                hours NUMERIC,
-                                                                FOREIGN KEY (group_id) REFERENCES groups(id),
-                                                                FOREIGN KEY (subject_id) REFERENCES subjects(id),
-                                                                FOREIGN KEY (class_type_id) REFERENCES class_types(id)
-                                                                )''')
-
-        self.main_cursor.execute('''CREATE TABLE IF NOT EXISTS timesheet (
-                                                                id NUMERIC PRIMARY KEY ASC,
-                                                                room_id NUMERIC,
-                                                                lecturer_id NUMERIC,
-                                                                group_id NUMERIC,
-                                                                class_type_id NUMERIC,
-                                                                subject_id NUMERIC,
-                                                                date DATE,
-                                                                class_number NUMERIC,
-                                                                FOREIGN KEY (room_id) REFERENCES rooms(id),
-                                                                FOREIGN KEY (lecturer_id) REFERENCES lecturers(personal_id),
-                                                                FOREIGN KEY (group_id) REFERENCES groups(id),
-                                                                FOREIGN KEY (subject_id) REFERENCES subjects(id),
-                                                                FOREIGN KEY (class_type_id) REFERENCES class_types(id)
-                                                                )''')
-
+        with open('schema.sql', 'r', encoding='utf-8') as file:
+            schema = file.read()
+        self.main_cursor.executescript(schema)
         self.main_cursor.execute(f"INSERT INTO user_data(login, password, role) VALUES('admin', "
                                  f"'{sha256('admin'.encode('utf-8')).hexdigest()}', 'admin')")
         self.main_cursor.execute(f"INSERT INTO user_data(login, password, role) VALUES('laborant', "
